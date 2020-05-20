2a7d79a3f36e1161587553d30dd13b9088567381
Dajan Radovic <dajo1986@gmail.com>
2020-05-20 18:42:28 +0200

    production build

---
+++ build/precache-manifest.747b582716864ca6f328ac7395e22c3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01ae3483b5517f3466e1c36518fb9c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01d3fc845cfafafc3cf6",
+    "url": "/static/css/2.d870dd77.chunk.css"
+  },
+  {
+    "revision": "9589bd6eecd10f11cd10",
+    "url": "/static/css/main.8cbdd51f.chunk.css"
+  },
+  {
+    "revision": "01d3fc845cfafafc3cf6",
+    "url": "/static/js/2.79d16fef.chunk.js"
+  },
+  {
+    "revision": "93760f5a748cf1fa6b2367f39fd686ea",
+    "url": "/static/js/2.79d16fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9589bd6eecd10f11cd10",
+    "url": "/static/js/main.004b25cf.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "c6d45945913f6d1a8238b54561f5bb8b",
+    "url": "/static/media/welcome1.c6d45945.jpeg"
+  }
+]);
